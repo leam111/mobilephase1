9a7f6850a328ff4983b26c33c617d484c485febd
Léa m <104256935+leam111@users.noreply.github.com>
2025-11-21 15:14:45 +0200

    list view with pictures

--- test/lib/ListViewDemo.dart
+++ test/lib/ListViewDemo.dart
@@ -7,58 +7,60 @@ class ListViewDemo extends StatefulWidget {
   State<ListViewDemo> createState() => _ListViewDemoState();
 }
 
-
-
 class _ListViewDemoState extends State<ListViewDemo> {
-
-
-final List<Map<String,String>>item=[
- {"Title":"CARTIER EYEWEAR","Subtitle":"400"}
-,{"Title":"MIU MIU","Subtitle":"362"}
-,{"Title":"PRADA EYEWEAR","Subtitle":"520"}
-,{"Title":"OLIVER PEOPLES","Subtitle":"390"}
-,{"Title":"LOEWE EYEWEAR","Subtitle":"640"}
-,{"Title":"LOEWE EYEWEAR","Subtitle":"640"}
-
-];
-
-
-
+  final List<Map<String, String>> item = [
+    {
+      "Title": "CARTIER EYEWEAR",
+      "Subtitle": "400",
+      "Image": "assets/img1.jpeg",
+    },
+    {"Title": "MIU MIU", "Subtitle": "362", "Image": "assets/img2.jpeg"},
+    {"Title": "PRADA EYEWEAR", "Subtitle": "520", "Image": "assets/img3.jpeg"},
+    {"Title": "OLIVER PEOPLES", "Subtitle": "390", "Image": "assets/img4.jpeg"},
+    {"Title": "LOEWE EYEWEAR", "Subtitle": "640", "Image": "assets/img5.jpeg"},
+    {"Title": "CAT EYEWEAR", "Subtitle": "740", "Image": "assets/img7.jpeg"},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-backgroundColor:Color(0xFFF5F5DC) ,
+      backgroundColor: Color.fromARGB(255, 255, 255, 237),
 
-body:ListView.builder(itemCount: item.length
-,itemBuilder: (context,index){
-return
- Center(child: Padding(padding: EdgeInsetsGeometry.all(20),
- child: Column(children: [SizedBox(height: 10,) 
- ,SizedBox(width: 400,height: 100,
- child: Card(color: Color.fromARGB(255, 250, 246, 226),
- child: ListTile(title: Text(item[index]['Title']!,style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
- ),subtitle: Text('\$${item[index]['Subtitle']! }',style: TextStyle(color: Colors.black),)
-,)
- 
- 
- 
- ,)
- ,)
- ]
- ,)
- ,)
- ,);
-
-
-
-
-
-})
-);
-
-
-
+      body: ListView.builder(
+        itemCount: item.length,
+        itemBuilder: (context, index) {
+          return Center(
+            child: Padding(
+              padding: EdgeInsetsGeometry.all(20),
+              child: Column(
+                children: [
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: 400,
+                    height: 100,
+                    child: Card(
+                      color: Color.fromARGB(255, 250, 246, 226),
+                      child: ListTile(
+                        title: Text(
+                          item[index]['Title']!,
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 0, 0, 0),fontStyle: FontStyle.italic
+                          ),
+                        ),
+                        subtitle: Text(
+                          '\$${item[index]['Subtitle']!}',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        leading: Image.asset(item[index]["Image"]!,width: 160),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
